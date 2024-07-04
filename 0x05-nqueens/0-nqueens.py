e874d88be8da060accb3be55396c7cf5a49b71e9
Destiny-Kay <destinykevogo@gmail.com>
2024-07-04 17:14:20 +0300

    Pycodestyle fix

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -4,14 +4,16 @@ A solution for the N-queens problem
 """
 import sys
 
+
 def nqueens(N):
     """
-    Returns all the possible ways N number of queens can be arranged on a chessboard
+    Returns all the possible ways N number of queens can be
+    arranged on a chessboard
     without any queen attacking another.
-    
+
     Args:
         N: Size of the chessboard and the number of queens.
-    
+
     Returns:
         List of solutions, each solution is a list of queen positions.
     """
@@ -19,10 +21,11 @@ def nqueens(N):
     solve_n_queens(N, 0, [], solutions)
     return solutions
 
+
 def solve_n_queens(N, row, current_solution, solutions):
     """
     A utility function to solve the problem.
-    
+
     Args:
         N: Size of the chessboard and the number of queens.
         row: Current row to place the queen.
@@ -38,32 +41,36 @@ def solve_n_queens(N, row, current_solution, solutions):
             solve_n_queens(N, row + 1, current_solution, solutions)
             current_solution.pop()
 
+
 def is_safe(row, col, current_solution):
     """
     Checks whether a queen is under attack in a particular cell on the board.
-    
+
     Args:
         row: Row to be checked.
         col: Column to be checked.
         current_solution: List of current positions of queens.
-    
+
     Returns:
         True if the position is safe, otherwise False.
     """
     for queen_row, queen_col in current_solution:
-        if queen_row == row or queen_col == col or abs(queen_row - row) == abs(queen_col - col):
+        if queen_row == row or queen_col == col or \
+         abs(queen_row - row) == abs(queen_col - col):
             return False
     return True
+
 
 def print_solutions(solutions):
     """
     Prints all the solutions in the required format.
-    
+
     Args:
         solutions: List of solutions to be printed.
     """
     for solution in solutions:
         print(solution)
+
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
@@ -79,6 +86,6 @@ if __name__ == "__main__":
     if N < 4:
         print("N must be at least 4")
         exit(1)
-    
+
     solutions = nqueens(N)
     print_solutions(solutions)
