a35e78eb9da06791df0eb4a1de8320a5b78dc212
Destiny-Kay <destinykevogo@gmail.com>
2024-07-11 16:55:14 +0300

    Fix errors

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-import request from 'request';
+const request = require('request')
 
 
 request(`https://swapi-api.alx-tools.com/api/films/${process.argv[2]}`, function (err, _, body) {
